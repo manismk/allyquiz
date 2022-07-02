9b9230bf09561a8cee0bf51d53da7db21cf9d674
Manikandan <mani@Manikandans-MacBook-Pro.local>
2022-07-02 19:15:06 +0530

    chore: react- typescrit-chakra initial setup -cleaned

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider, Box, Text, Grid, theme } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <Text>Hello World</Text>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+);
