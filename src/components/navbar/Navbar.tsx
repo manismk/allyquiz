9203f56d18cf1407312fb90597d5d4b9f377c1a4
manismk <manismkmech@gmail.com>
2022-07-30 18:09:35 +0530

    feat: login added to the navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,25 @@
-import { Flex, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Button,
+  Flex,
+  Icon,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  Text,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { useAppSelector } from "../../app/hooks";
 import { ColorModeSwitcher } from "../../ColorModeSwitcher";
+import { BiLogIn } from "react-icons/bi";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase";
 
 export const Navbar = () => {
+  const user = useAppSelector((state) => state.auth.user);
+
   return (
     <Flex
       bg="primary.dark"
@@ -19,7 +36,54 @@ export const Navbar = () => {
           A11y Quiz
         </Text>
       </Link>
-      <ColorModeSwitcher justifySelf="flex-end" />
+      <Flex alignItems="center" gap="1rem">
+        <ColorModeSwitcher />
+        {user ? (
+          <Flex flexDirection="column" alignItems="center">
+            <Popover>
+              <PopoverTrigger>
+                <Button
+                  p="0"
+                  minW="unset"
+                  h="unset"
+                  background="transparent"
+                  _hover={{ background: "transparent" }}
+                >
+                  <Avatar
+                    size="sm"
+                    name={user.displayName || undefined}
+                    src={user.photoURL || undefined}
+                  />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent w="6.5rem">
+                <PopoverArrow />
+                <PopoverBody>
+                  <Button
+                    background="primary.light"
+                    _hover={{ background: "primary.light" }}
+                    onClick={() => {
+                      signOut(auth)
+                        .then(() => {
+                          // Sign-out successful.
+                        })
+                        .catch((error) => {});
+                    }}
+                  >
+                    Logout
+                  </Button>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
+          </Flex>
+        ) : (
+          <Link to="/login">
+            <Flex flexDirection="column" alignItems="center">
+              <Icon as={BiLogIn} fontSize="2rem" />
+            </Flex>
+          </Link>
+        )}
+      </Flex>
     </Flex>
   );
 };
