004acfd96ea5bb887df0132d1664d0bc77a7df28
manismk <manismkmech@gmail.com>
2022-10-07 22:07:28 +0530

    feat: original questions added for quiz

--- src/data/quiz.ts
+++ src/data/quiz.ts
@@ -32,130 +32,134 @@ export const Quizes: quizesType = {
       questions: [
         {
           questionNo: 1,
-          question: "This is question 1",
+          question: "What is the full form of WCAG?",
           options: [
             {
               isRight: false,
-              option: "option 1",
+              option: "Web consortium for Accessibility Guidelines",
               optionNo: 1,
             },
             {
-              isRight: true,
-              option: "option 2",
+              isRight: false,
+              option: "World computer Accessibility Guidelines",
               optionNo: 2,
             },
             {
-              isRight: false,
-              option: "option 3",
+              isRight: true,
+              option: "Web Content Accessibility Guidelines",
               optionNo: 3,
             },
             {
               isRight: false,
-              option: "option 4",
+              option: "Web and computer Accessibility Guidelines",
               optionNo: 4,
             },
           ],
         },
         {
           questionNo: 2,
-          question: "This is question 2",
+          question:
+            "Which of the following is not a type of cognitive disability?",
           options: [
             {
               isRight: false,
-              option: "option 1",
+              option: "Dyslexia",
               optionNo: 1,
             },
             {
               isRight: true,
-              option: "option 2",
+              option: "Blind",
               optionNo: 2,
             },
             {
               isRight: false,
-              option: "option 3",
+              option: "Autism",
               optionNo: 3,
             },
             {
               isRight: false,
-              option: "option 4",
+              option: "memory loss",
               optionNo: 4,
             },
           ],
         },
         {
           questionNo: 3,
-          question: "This is question 3",
+          question: "What is NVDA?",
           options: [
             {
               isRight: false,
-              option: "option 1",
+              option: "Speech recognition software ",
               optionNo: 1,
             },
             {
-              isRight: true,
-              option: "option 2",
+              isRight: false,
+              option: "Accessibility organization",
               optionNo: 2,
             },
             {
               isRight: false,
-              option: "option 3",
+              option: "An initiative by W3C.",
               optionNo: 3,
             },
             {
-              isRight: false,
-              option: "option 4",
+              isRight: true,
+              option: "A screen reader",
               optionNo: 4,
             },
           ],
         },
         {
           questionNo: 4,
-          question: "This is question 4",
+          question:
+            "How can we make an image accessible to someone who can’t see it?",
           options: [
             {
               isRight: false,
-              option: "option 1",
+              option: "Don't use images in the website",
               optionNo: 1,
             },
             {
-              isRight: true,
-              option: "option 2",
+              isRight: false,
+              option: "Images by default accessible by screen raeders",
               optionNo: 2,
             },
             {
-              isRight: false,
-              option: "option 3",
+              isRight: true,
+              option:
+                "Add proper alternative text that descrives image in 'alt' attribute",
               optionNo: 3,
             },
             {
               isRight: false,
-              option: "option 4",
+              option: "Remove the alt attribute from image tag.",
               optionNo: 4,
             },
           ],
         },
         {
           questionNo: 5,
-          question: "This is question 5",
+          question: "Who benefits from the video captions provided",
           options: [
             {
               isRight: false,
-              option: "option 1",
+              option: "A deaf person",
               optionNo: 1,
             },
             {
-              isRight: true,
-              option: "option 2",
+              isRight: false,
+              option: "A person in a noisy environment",
               optionNo: 2,
             },
             {
               isRight: false,
-              option: "option 3",
+              option:
+                " People with cognitive disabilities who benefit from reading text on screen while listening to it",
               optionNo: 3,
             },
             {
-              isRight: false,
-              option: "option 4",
+              isRight: true,
+              option: "All the above",
               optionNo: 4,
             },
           ],
@@ -170,130 +174,134 @@ export const Quizes: quizesType = {
       questions: [
         {
           questionNo: 1,
-          question: "This is question 1",
+          question: "Which of the following is not a screen reader?",
           options: [
             {
               isRight: false,
-              option: "option 1",
+              option: "NVDA",
               optionNo: 1,
             },
             {
               isRight: true,
-              option: "option 2",
+              option: "SIRI",
               optionNo: 2,
             },
             {
               isRight: false,
-              option: "option 3",
+              option: "JAWS",
               optionNo: 3,
             },
             {
               isRight: false,
-              option: "option 4",
+              option: "Voice Over",
               optionNo: 4,
             },
           ],
         },
         {
           questionNo: 2,
-          question: "This is question 2",
+          question:
+            "Which of the following not fall under POUR accessibility principles?",
           options: [
             {
-              isRight: false,
-              option: "option 1",
+              isRight: true,
+              option: "Undesirable",
               optionNo: 1,
             },
             {
-              isRight: true,
-              option: "option 2",
+              isRight: false,
+              option: "Robust",
               optionNo: 2,
             },
             {
               isRight: false,
-              option: "option 3",
+              option: "Perceivable",
               optionNo: 3,
             },
             {
               isRight: false,
-              option: "option 4",
+              option: "Operable",
               optionNo: 4,
             },
           ],
         },
         {
           questionNo: 3,
-          question: "This is question 3",
+          question:
+            "A person have both vision disability and hearing disability. Which of the assitive technology will be used by him?",
           options: [
             {
               isRight: false,
-              option: "option 1",
+              option: "Alternative Keyboards and Mice. ",
               optionNo: 1,
             },
             {
-              isRight: true,
-              option: "option 2",
+              isRight: false,
+              option: "Speech recognition software",
               optionNo: 2,
             },
             {
               isRight: false,
-              option: "option 3",
+              option: "Screen readers",
               optionNo: 3,
             },
             {
-              isRight: false,
-              option: "option 4",
+              isRight: true,
+              option: "Braille Support",
               optionNo: 4,
             },
           ],
         },
         {
           questionNo: 4,
-          question: "This is question 4",
+          question:
+            "A normal text should have which of the following color contrast ratio to meet the WCAG AA guidelines?",
           options: [
             {
               isRight: false,
-              option: "option 1",
+              option: "3:1",
               optionNo: 1,
             },
             {
               isRight: true,
-              option: "option 2",
+              option: "4.5:1",
               optionNo: 2,
             },
             {
               isRight: false,
-              option: "option 3",
+              option: "7:1",
               optionNo: 3,
             },
             {
               isRight: false,
-              option: "option 4",
+              option: "5:1",
               optionNo: 4,
             },
           ],
         },
         {
           questionNo: 5,
-          question: "This is question 5",
+          question: "Why you need to consider making your website accessible?",
           options: [
             {
               isRight: false,
-              option: "option 1",
+              option: "To avoid legal issues.",
               optionNo: 1,
             },
             {
-              isRight: true,
-              option: "option 2",
+              isRight: false,
+              option:
+                "Making accessible websites, brings more no of users to website",
               optionNo: 2,
             },
             {
               isRight: false,
-              option: "option 3",
+              option: "Accessibility makes website SEO friendly",
               optionNo: 3,
             },
             {
-              isRight: false,
-              option: "option 4",
+              isRight: true,
+              option: "All the above",
               optionNo: 4,
             },
           ],
